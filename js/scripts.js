8ec1b8339d82b72ec6d0221fbd9f7b9aec2bd786
Amir1116 <emd8391@gmail.com>
2021-07-19 23:03:04 +0300

    added show even number

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+
+// function 
+//========================get dom elements========
+const btnGetNum = document.querySelector('.get-number');
+const btnShowNum = document.querySelector('.show-number');
+const ouputUl = document.querySelector('.output__ul');
+const outNum = document.querySelector('.number-out');
+const numDescription = document.querySelector('.number-description');
+console.log(btnGetNum,btnShowNum);
+
+
+
+
+
+function getNumber(){    
+    const number = prompt('enter number', 'number 0-10');
+    if((/^[0-9]|10$/).test(number)&&(+number>=0&&+number<=10)){
+            elementText(number,'You Number!')
+    }else{
+        alert('number is not valid!');
+        elementText('Try again!','Your number is no valid!');
+        btnDisable(btnShowNum, btnGetNum);
+        
+    }    
+}
+
+function elementText(txt1,txt2){
+    outNum.textContent=txt1;
+    numDescription.textContent =txt2; 
+}
+
+function showNumber(n,el){
+    const fr = document.createDocumentFragment();
+    for(let i = 0;i<=n; i++){
+        if(i%2===0){
+            createLi(i,fr)
+        }       
+    }
+    el.appendChild(fr);
+}
+
+function createLi(n,el){
+    const li = document.createElement('li');
+        li.classList.add('output__item');
+        li.textContent = n;
+    el.appendChild(li);    
+}
+
+function btnDisable(btn1,btn2){
+    btn1.disabled = true;
+    btn2.disabled = false;
+}
+
+btnGetNum.addEventListener('click',(e)=>{
+    // e.target.disabled = true;
+    // btnShowNum.disabled = false;
+    btnDisable(e.target, btnShowNum);
+    getNumber()
+});
+btnShowNum.addEventListener('click',(e)=>{
+    ouputUl.innerHTML = '';
+   btnDisable(e.target,btnGetNum);
+   let num = Number(outNum.textContent);
+   console.log(num);
+    showNumber(num,ouputUl);
+});
